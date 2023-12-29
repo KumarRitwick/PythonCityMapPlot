cebadf95369858046f39faa843f6f5192a3b10bf
kumarritwick <kr.ritwick@gmail.com>
2023-12-29 16:12:07 +0000

    Filter function Added

--- CityPlot.py
+++ CityPlot.py
@@ -5,9 +5,26 @@ import matplotlib.pyplot as plt
 def load_data(file_path):
     return pd.read_csv(file_path)
 
+#This function will filter the data based on requirement
+#i.e. mentioned in the documnet
+def filter_location(location, long_range, lat_range):
+    return location[
+        (location['Longitude'].between(*long_range)) &
+        (location['Latitude'].between(*lat_range))
+    ]
+
+def plot_grow_data(location, map_path, output_path):
+    long_range = [50.681, 57.985]
+    lat_range = [-10.592, 1.6848]
+
+    filtered_location = filter_location(location, long_range, lat_range)
+
 def main():
     grow_location = load_data("GrowLocations.csv")
+    map_path = "map7.png"
+    output_path = "plots.png"
 
+    plot_grow_data(grow_location, map_path, output_path)
 
 if __name__ == '__main__':
     main()
